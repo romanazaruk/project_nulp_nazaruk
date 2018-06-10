0ec924a9530bf11f713e99080e1afa09750b4c74
romanazaruk <33019500+romanazaruk@users.noreply.github.com>
2018-06-10 23:30:09 +0300

    Update main.java

--- main.java
+++ main.java
@@ -4,29 +4,29 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		Kitchen Loft = new Kitchen(); 
+		Kitchen loft = new Kitchen(); 
 		
-		Kitchen Hi_Tech = new Kitchen("Hi_Tech", "gray", 4, 50.5); 
+		Kitchen hiTech = new Kitchen("Hi_Tech", "gray", 4, 50.5); 
 
-		Kitchen Provance = new Kitchen("Provance", "yellow", 1, 15.5,1 ); 
+		Kitchen provance = new Kitchen("Provance", "yellow", 1, 15.5,1 ); 
 		 
-		System.out.println(Loft.toString()); 
+		System.out.println(loft.toString()); 
 		
-		System.out.println(Hi_Tech.toString()); 
+		System.out.println(hiTech.toString()); 
 		
-		System.out.println(Provance.toString()); 
+		System.out.println(provance.toString()); 
 		
-		Loft.resetValues("Loft", "red", 3, 45,2); 
+		loft.resetValues("Loft", "red", 3, 45,2); 
 		
-		System.out.println(Loft.toString());
+		System.out.println(loft.toString());
 
-		Loft.printSum(); 
+		loft.printSum(); 
   
-		Hi_Tech.printSum(); 
+		hiTech.printSum(); 
  
-		Provance.printSum(); 
+		provance.printSum(); 
  
-		Hi_Tech.setNumberOfDrawers(5); 
+		hiTech.setNumberOfDrawers(5); 
  
 		Kitchen.printStaticSum(); 
 		} 
